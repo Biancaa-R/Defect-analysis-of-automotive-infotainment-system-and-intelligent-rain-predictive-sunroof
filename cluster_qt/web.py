144b97a651ea38c83b170bf5c22653e75b0a853b
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2024-03-07 00:19:25 +0530

    Delete cluster_qt/web.py

--- cluster_qt/web.py
+++
@@ -1,8 +0,0 @@
-# first import the module 
-import webbrowser 
-
-# then make a url variable 
-url = "https://www.google.com/maps"
-
-# then call the default open method described above 
-webbrowser.open(url)
